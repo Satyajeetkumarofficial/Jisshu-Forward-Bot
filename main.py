26edc3f8bfab029b9096b36bc705b7d2a225f8bc
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-05-10 17:20:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 # bot developer @mr_jisshu
 from bot import Bot
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 app = Bot()
 app.run()
